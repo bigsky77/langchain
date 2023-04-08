87f2a365586cbe657ef1f49476967f7282246227
Simon <simonjudd2@gmail.com>
2023-04-08 11:32:21 +0100

    fix: fix: Modify TelegramChatLoader to default to including date, text, and from, with the ability to include only text based on a parameter.

--- langchain/document_loaders/telegram.py
+++ langchain/document_loaders/telegram.py
@@ -18,9 +18,12 @@ def concatenate_rows(row: dict) -> str:
 class TelegramChatLoader(BaseLoader):
     """Loader that loads Telegram chat json directory dump."""
 
-    def __init__(self, path: str):
+    def __init__(self, path: str, include: Optional[List[str]] = None):
         """Initialize with path."""
         self.file_path = path
+        if include is None:
+            include = ["date", "text", "from"]
+        self.include = include
 
     def load(self) -> List[Document]:
         """Load documents."""
@@ -45,10 +48,10 @@ class TelegramChatLoader(BaseLoader):
             & (df_normalized_messages.text.apply(lambda x: type(x) == str))
         ]
 
-        df_filtered = df_filtered[["date", "text", "from"]]
+        df_filtered = df_filtered[self.include]
 
         text = df_filtered.apply(concatenate_rows, axis=1).str.cat(sep="")
 
-        metadata = {"HELLO": str(p)}
+        metadata = {"source": str(p)}
 
         return [Document(page_content=text, metadata=metadata)]
